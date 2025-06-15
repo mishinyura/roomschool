9d9f882b8a0d2aa2dd5eed64f9d29d1e15b3ecc5
Yura Mishin <robothot@yandex.ru>
2025-06-15 18:08:23 +0300

    Добавил бургер и hero, поправил стили и шрифты, наполнил текстом, добавил анимацию

--- frontend/scripts/app.js
+++ frontend/scripts/app.js
@@ -1,21 +1,52 @@
-function main() {
-    let footer = document.querySelector('.footer')
-
+function showPresentText(footerElement){
     const observer = new IntersectionObserver(entries => {
     entries.forEach(entry => {
         let ratio = Math.min(1, Math.max(0, entry.intersectionRatio));
-        footer.style.setProperty('--opacity-footer-after-before', ratio.toFixed(2));
+        footerElement.style.setProperty('--opacity-footer-after-before', ratio.toFixed(2));
         if (ratio != 0) {
-            footer.style.setProperty('--visibility-footer-arter-before', 'visible')
+            footerElement.style.setProperty('--visibility-footer-arter-before', 'visible')
         } else {
-            footer.style.setProperty('--visibility-footer-arter-before', 'hidden')
+            footerElement.style.setProperty('--visibility-footer-arter-before', 'hidden')
         }
     });
     }, {
         threshold: Array.from({ length: 101 }, (_, i) => i / 100)
     });
 
-    observer.observe(footer);
+    observer.observe(footerElement);
+};
+
+function editViewTabs(courses, text) {
+    for (let course of courses) {
+        course.addEventListener('mouseover', (elem) => {
+            let parent = elem.target.closest(`.${course.classList[0]}`)
+            let courseName = parent.dataset.course
+            for (let txt of text) {
+                txt.classList.remove('show')
+                if (txt.dataset.course === courseName) {
+                    txt.classList.add('show')
+                }
+            };
+        })
+    }
+}
+
+function editViewBurger(burger) {
+    console.log(burger)
+    burger.addEventListener('click', () => {
+        burger.classList.toggle('open')
+    })
+}
+
+function main() {
+    let footer = document.querySelector('.footer');
+    let coursesCard = document.querySelectorAll('.services__course');
+    let coursesText = document.querySelectorAll('.services__text');
+    let burgerParent = document.querySelector('.panel__item_burger');
+    let burgerBtn = burgerParent.querySelector('button');
+    showPresentText(footer)
+    editViewTabs(coursesCard, coursesText)
+    editViewBurger(burgerBtn)
 }
 
 main()
