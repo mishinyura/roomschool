614b3047fb7646024b5f29ad28db0872f4433505
Yura Mishin <robothot@yandex.ru>
2025-06-15 22:54:35 +0300

    Добавил страницу авторизации, задал начальные стили

--- frontend/scripts/app.js
+++ frontend/scripts/app.js
@@ -38,15 +38,54 @@ function editViewBurger(burger) {
     })
 }
 
+function request(){
+    return ['admin', 'employee', 'parent']
+}
+
+
+function editViewLogin(btn) {
+    let form = btn.closest('form')
+    btn.addEventListener('click', () => {
+        //Отправляем данные на сервер, 
+        //чтобы получить разрешение, 
+        //если сервер возвращает список,
+        //формируем список доступных аккаунтов и даем выбор
+        let response = request()
+        form = new FormData(form)
+        console.log(form)
+        if (response.length > 1) {
+            let accountsContainer = document.querySelector('.choise');
+            let authContainer = document.querySelector('.auth');
+            let accounts = accountsContainer.querySelectorAll('.choise__item');
+            for (let acc of accounts) {
+                if (response.includes(acc.dataset.account)) {
+                    acc.classList.add('active')
+                }
+            }
+            accountsContainer.classList.add('active')
+            authContainer.classList.remove('active')
+        } else {
+            //если аккаунт 1, то редиректим сразу на него
+            console.log('Redirect on profile')
+        }
+    })
+}
+
 function main() {
-    let footer = document.querySelector('.footer');
-    let coursesCard = document.querySelectorAll('.services__course');
-    let coursesText = document.querySelectorAll('.services__text');
-    let burgerParent = document.querySelector('.panel__item_burger');
-    let burgerBtn = burgerParent.querySelector('button');
-    showPresentText(footer)
-    editViewTabs(coursesCard, coursesText)
-    editViewBurger(burgerBtn)
+    // let footer = document.querySelector('.footer');
+    // let coursesCard = document.querySelectorAll('.services__course');
+    // let coursesText = document.querySelectorAll('.services__text');
+    // let burgerParent = document.querySelector('.panel__item_burger');
+    // let burgerBtn = burgerParent.querySelector('button');
+    // let linksMenu = document.querySelectorAll('.topmenu__link');
+    let loginBtn = document.querySelector('.auth__bth_login');
+
+
+    // showPresentText(footer)
+    // editViewTabs(coursesCard, coursesText)
+    // editViewBurger(burgerBtn)
+    // editViewSubmenu(linksMenu)
+    editViewLogin(loginBtn)
 }
 
 main()
