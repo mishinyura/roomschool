9b0469078a7a88b0dc8fd4374a560eee2560d800
Yura Mishin <robothot@yandex.ru>
2025-09-25 22:51:05 +0300

    Внес правки в стилизацию страницы с курсами

--- frontend/pages/courses/app.js
+++ frontend/pages/courses/app.js
@@ -1,5 +1,14 @@
+import { Select } from '../../assets/scripts/components/Select.js'
+import { Header } from '../../assets/scripts/modules/Header.js'
+
 function main() {
-    
+    const header = new Header('.header')
+
+    const sortedSelect = new Select({
+        obj: '.filters__select',
+        input: '.filters__wrapper',
+        options: '.filters__sort'
+    })
 }
 
 document.addEventListener("DOMContentLoaded", () => {
