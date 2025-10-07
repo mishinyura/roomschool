de743fad38e891e2dccf167bb46cbcccaafcbe0b
Yura Mishin <robothot@yandex.ru>
2025-10-07 20:14:48 +0300

    Внес правки в дизайн хедера при десктопе

--- frontend/assets/scripts/components/Select.js
+++ frontend/assets/scripts/components/Select.js
@@ -4,15 +4,15 @@
  * список, регулирует показ списка и управляет выбором
  */
 
-export class Select{
+export class Select {
     constructor(settings) {
         this.container = document.querySelector(settings.obj)
         this.wrapper = this.container.querySelector(settings.input)
-        
+
         this.selected = this.wrapper.querySelector('input')
         this.choices = this.container.querySelectorAll(settings.options)
         this.list = this.choices[0].parentElement
-        
+
         this.handlerShowList = this.showList.bind(this)
         this.handlerHideList = this.hideList.bind(this)
 
@@ -36,7 +36,7 @@ export class Select{
     }
 
     showList() {
-        if (!this.list.classList.contains('active')){
+        if (!this.list.classList.contains('active')) {
             this.list.classList.add('active')
         }
         this.handleOutside = (e) => this.closeIfOutside(e);
@@ -55,3 +55,48 @@ export class Select{
         this.setChoice(elem.target)
     }
 }
+import { createElement } from '../utils.js'
+
+// class SelectBuilder{
+//     createSelector() {
+//         let selector = createElement('div', 'selector')
+
+//         return selector
+//     }
+
+//     createBtn() {
+//         let wrapper = createElement('label', 'selector__wrapper')
+//         let input = createElement('input', 'selector__input', {'name': 'method'})
+//     }
+
+//     createSelect(lst) {
+//         this.container.classList.add('selector')
+        
+//         let lst = createElement('ul', 'selector__list')
+
+//         for (let item of lst) {
+//             let li = document.createElement('li', 'selector__item')
+//             li.innerText = item
+//             lst.appendChild(li)
+//         }
+//     }
+// }
+
+// export class Select {
+//     constructor(settings) {
+//         this.container = document.querySelector(settings.obj)
+        
+//         this.items = settings.items ? settings.items : new Error('Select: items not found')
+//         this.activeItem = this.items[settings.default] ? settings.default && settings.items : 0
+
+//         console.log(this.container, this.items, this.activeItem)
+//     }
+
+//     open() {
+//         this.container.classList.add('open')
+//     }
+
+//     close() {
+//         this.container.classList.remove('open')
+//     }
+// }
